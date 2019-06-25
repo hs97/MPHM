646f53ac42c2bde05cbe3279c3697f9169bfac09
hs97 <hs9@williams.edu>
2019-06-25 13:46:21 -0400

    hc bb

--- R/bry_boschan.R
+++ R/bry_boschan.R
@@ -1,6 +1,21 @@
 #' @import BCDating
+#' @import zoo
 #' @export
 
-bry_boschan <- function(dat.country, mincycle = 5, minphase = 2) {
-  BBQ(dat.country, mincycle, minphase, name="Dating Business Cycles")
+bry_boschan <- function(dat, country, mincycle = 5, minphase = 1) {
+  dat_country <- dat %>%
+    select("date", ends_with(country)) %>%
+    na.omit()
+  dat_country_ts <- ts(select(dat_country, ends_with(country)), start = dat_country$date[1], freq = 4)
+  try(
+    {bb <- BBQ(dat_country_ts, mincycle, minphase)@states
+    res <- data.frame(date = time(bb), state = as.matrix(bb)) %>%
+      mutate(date = as.yearqtr(date)) %>%
+      right_join(y = dat_country, by = "date") %>%
+      setNames(gsub("state",paste("state", country, sep = "_"), names(.)))
+    return(res)},
+    silent = TRUE)
+  dat_country %>%
+    mutate(state = 1) %>%
+    setNames(gsub("state",paste("state", country, sep = "-"), names(.)))
 }
